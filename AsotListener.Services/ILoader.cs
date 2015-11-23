787045f4ee58d1f7dc5dc977352c4fefd6e205c4
Pavlo Oleshkevych <oleshkevych@gmail.com>
2015-11-23 09:11:30 +0200

    Introduced services library. Created interface for Loader. Added logger to Loader.

---
+++ AsotListener.Services/ILoader.cs
@@ -0,0 +1,14 @@
+﻿namespace AsotListener.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Models;
+
+    public interface ILoader: IDisposable
+    {
+        Task<string> FetchEpisodeListAsync();
+        Task<string> FetchEpisodePageAsync(Episode episode);
+        Task DownloadEpisodeAsync(ICollection<string> urls, string filename);
+    }
+}
--- AsotListener.Services/Loader.cs
+++ AsotListener.Services/Loader.cs
@@ -1,4 +1,4 @@
-﻿namespace AsotListener
+﻿namespace AsotListener.Services
 {
     using System;
     using System.Collections.Generic;
@@ -6,8 +6,9 @@
     using System.Net.Http;
     using System.Threading.Tasks;
     using Models;
+    using Windows.Foundation.Diagnostics;
 
-    class Loader : IDisposable
+    public class Loader : ILoader
     {
         private const string MAIN_URL = "http://asotarchive.org";
         private const string FALLBACK_FILENAME = "file";
@@ -15,25 +16,31 @@
         private const int CONNECTION_TIMEOUT_SECONDS = 30;
 
         private HttpClient httpClient;
+        private LoggingChannel logChannel;
 
-        public Loader()
+        public Loader(LoggingChannel logChannel)
         {
             httpClient = new HttpClient();
             httpClient.Timeout = TimeSpan.FromSeconds(CONNECTION_TIMEOUT_SECONDS);
+            logChannel = new LoggingChannel("LoaderLogChannel");
         }
 
         public async Task<string> FetchEpisodeListAsync()
         {
+            logChannel.LogMessage("Fetching episode list.", LoggingLevel.Verbose);
             return await httpClient.GetStringAsync(MAIN_URL);
         }
 
         public async Task<string> FetchEpisodePageAsync(Episode episode)
         {
+            logChannel.LogMessage("Fetching episode page.", LoggingLevel.Verbose);
             return await httpClient.GetStringAsync(MAIN_URL + episode.Url);
         }
 
-        public async Task DownloadEpisodeAsync(List<string> urls, string filename)
+        public async Task DownloadEpisodeAsync(ICollection<string> urls, string filename)
         {
+            string message;
+
             if (urls == null || urls.Count < 1)
             {
                 return;
@@ -48,11 +55,12 @@
             {
                 int fileId = i + 1;
                 string episodeFilename = Path.GetFullPath(filename + fileId.ToString() + FILE_EXTENSION);
-                Console.WriteLine(
+                message = string.Format(
                     "Starting download part {0} of {1} into {2} file",
                     fileId,
                     urls.Count,
                     episodeFilename);
+                logChannel.LogMessage(message, LoggingLevel.Verbose);
 
                 using (HttpResponseMessage response = await httpClient.GetAsync(
                     urls[i],
@@ -60,13 +68,16 @@
                 {
                     if (!response.IsSuccessStatusCode)
                     {
-                        Console.WriteLine("Server connection error. Status {0} {1}",
-                            response.StatusCode, 
+                        message = string.Format(
+                            "Server connection error. Status {0} {1}",
+                            response.StatusCode,
                             response.ReasonPhrase);
+                        logChannel.LogMessage(message, LoggingLevel.Error);
                     }
-                    
-                    Console.WriteLine("Starting download {0} bytes", 
+
+                    message = string.Format("Have to download {0} bytes", 
                         response.Content.Headers.ContentLength);
+                    logChannel.LogMessage(message, LoggingLevel.Verbose);
                     using (Stream streamToReadFrom = await response.Content.ReadAsStreamAsync())
                     {
                         string fileToWriteTo = Path.GetTempFileName();
@@ -74,9 +85,9 @@
                             fileToWriteTo, 
                             FileMode.Create))
                         {
-                            Console.WriteLine("Start loading...");
+                            logChannel.LogMessage("Download started.", LoggingLevel.Verbose);
                             await streamToReadFrom.CopyToAsync(streamToWriteTo);
-                            Console.WriteLine("Load complete.");
+                            logChannel.LogMessage("Download complete.", LoggingLevel.Verbose);
                         }
                     }
                 }
@@ -85,6 +96,7 @@
 
         public void Dispose()
         {
+            logChannel.Dispose();
             httpClient.Dispose();
         }
     }
