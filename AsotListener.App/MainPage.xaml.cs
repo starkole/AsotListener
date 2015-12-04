f35578a7a9cae898d9cecf185a255e51c527aeb8
Pavlo Oleshkevych <oleshkevych@gmail.com>
2015-12-04 11:31:15 +0200

    Filled up Player view model.

--- AsotListener.App/MainPage.xaml.cs
+++ AsotListener.App/MainPage.xaml.cs
@@ -8,67 +8,17 @@
     using Windows.Foundation.Diagnostics;
     using System;
     using Models;
-    using System.Threading;
-    using Windows.Media.Playback;
     using Services;
-    using Windows.Foundation.Collections;
-    using Windows.Foundation;
-    using Windows.UI.Core;
-    using System.Diagnostics;
 
     public sealed partial class MainPage : Page, IDisposable
     {
         private readonly NavigationHelper navigationHelper;
         private readonly MainPageViewModel mainPageViewModel;
-
-        private AutoResetEvent ServerInitialized;
-        private bool isMyBackgroundTaskRunning = false;
+        private readonly PlayerViewModel playerModel;        
         private IApplicationSettingsHelper applicationSettingsHelper = ApplicationSettingsHelper.Instance;
 
         //private readonly ResourceLoader resourceLoader = ResourceLoader.GetForCurrentView("Resources");
-        private readonly ILoggingSession loggingSession;
-
-        /// <summary>
-        /// Gets the information about background task is running or not by reading the setting saved by background task
-        /// </summary>
-        private bool IsMyBackgroundTaskRunning
-        {
-            get
-            {
-                if (isMyBackgroundTaskRunning)
-                    return true;
-
-                object value = applicationSettingsHelper.ReadSettingsValue(Constants.BackgroundTaskState);
-                if (value == null)
-                {
-                    return false;
-                }
-                else
-                {
-                    isMyBackgroundTaskRunning = ((String)value).Equals(Constants.BackgroundTaskRunning);
-                    return isMyBackgroundTaskRunning;
-                }
-            }
-        }
-
-        /// <summary>
-        /// Read current track information from application settings
-        /// </summary>
-        private string CurrentTrack
-        {
-            get
-            {
-                object value = applicationSettingsHelper.ReadSettingsValue(Constants.CurrentTrack);
-                if (value != null)
-                {
-                    return (string)value;
-                }
-                else
-                {
-                    return string.Empty;
-                }
-            }
-        }
+        private readonly ILoggingSession loggingSession;        
 
         public MainPage()
         {
@@ -79,6 +29,7 @@
             this.loggingSession = (LoggingSession)Application.Current.Resources[Constants.LOGGING_SESSION_NAME];
 
             this.mainPageViewModel = new MainPageViewModel(this.loggingSession);
+            this.playerModel = new PlayerViewModel();
 
             this.InitializeComponent();
 
@@ -86,9 +37,7 @@
 
             this.navigationHelper = new NavigationHelper(this);
             this.navigationHelper.LoadState += mainPageViewModel.OnNavigationHelperLoadState;
-            this.navigationHelper.SaveState += mainPageViewModel.OnNavigationHelperSaveState;
-
-            ServerInitialized = new AutoResetEvent(false);
+            this.navigationHelper.SaveState += mainPageViewModel.OnNavigationHelperSaveState;            
         }
 
         /// <summary>
@@ -96,10 +45,8 @@
         /// </summary>
         public NavigationHelper NavigationHelper => this.navigationHelper;
 
-        /// <summary>
-        /// Gets the view model for this <see cref="Page"/>.
-        /// </summary>
         public MainPageViewModel MainPageViewModel => this.mainPageViewModel;
+        public PlayerViewModel PlayerModel => this.playerModel;
 
         #region NavigationHelper registration
 
@@ -119,69 +66,9 @@
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             this.navigationHelper.OnNavigatedTo(e);
-
-            //Adding App suspension handlers here so that we can unsubscribe handlers 
-            //that access to BackgroundMediaPlayer events
-            App.Current.Suspending += ForegroundApp_Suspending;
-            App.Current.Resuming += ForegroundApp_Resuming;
             applicationSettingsHelper.SaveSettingsValue(Constants.AppState, Constants.ForegroundAppActive);
         }
-
-        #region Foreground App Lifecycle Handlers
-        /// <summary>
-        /// Sends message to background informing app has resumed
-        /// Subscribe to MediaPlayer events
-        /// </summary>
-        void ForegroundApp_Resuming(object sender, object e)
-        {
-            applicationSettingsHelper.SaveSettingsValue(Constants.AppState, Constants.ForegroundAppActive);
-
-            // Verify if the task was running before
-            if (IsMyBackgroundTaskRunning)
-            {
-                //if yes, reconnect to media play handlers
-                AddMediaPlayerEventHandlers();
-
-                //send message to background task that app is resumed, so it can start sending notifications
-                ValueSet messageDictionary = new ValueSet();
-                messageDictionary.Add(Constants.AppResumed, DateTime.Now.ToString());
-                BackgroundMediaPlayer.SendMessageToBackground(messageDictionary);
-
-                if (BackgroundMediaPlayer.Current.CurrentState == MediaPlayerState.Playing)
-                {
-                    playButton.Content = "| |";     // Change to pause button
-                }
-                else
-                {
-                    playButton.Content = ">";     // Change to play button
-                }
-                txtCurrentTrack.Text = CurrentTrack;
-            }
-            else
-            {
-                playButton.Content = ">";     // Change to play button
-                txtCurrentTrack.Text = "";
-            }
-
-        }
-
-        /// <summary>
-        /// Send message to Background process that app is to be suspended
-        /// Stop clock and slider when suspending
-        /// Unsubscribe handlers for MediaPlayer events
-        /// </summary>
-        void ForegroundApp_Suspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-            ValueSet messageDictionary = new ValueSet();
-            messageDictionary.Add(Constants.AppSuspended, DateTime.Now.ToString());
-            BackgroundMediaPlayer.SendMessageToBackground(messageDictionary);
-            RemoveMediaPlayerEventHandlers();
-            applicationSettingsHelper.SaveSettingsValue(Constants.AppState, Constants.ForegroundAppSuspended);
-            deferral.Complete();
-        }
-        #endregion
-
+        
         protected override void OnNavigatedFrom(NavigationEventArgs e)
         {
             this.navigationHelper.OnNavigatedFrom(e);
@@ -196,15 +83,10 @@
             {
                 if (disposing)
                 {
-                    // TODO: dispose managed state (managed objects).
+                    Application.Current.Resources.Remove(Constants.LOGGING_SESSION_NAME);
+                    this.loggingSession.Dispose();
+                    this.playerModel.Dispose();
                 }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
-                // TODO: set large fields to null.
-                
-                Application.Current.Resources.Remove(Constants.LOGGING_SESSION_NAME);
-                this.loggingSession.Dispose();
-                this.ServerInitialized.Dispose();
 
                 disposedValue = true;
             }
@@ -227,186 +109,5 @@
                 this.MainPageViewModel.SelectedEpisode = (Episode)e.ClickedItem;
             }
         }
-
-        #region Background MediaPlayer Event handlers
-        /// <summary>
-        /// MediaPlayer state changed event handlers. 
-        /// Note that we can subscribe to events even if Media Player is playing media in background
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="args"></param>
-        async void MediaPlayer_CurrentStateChanged(MediaPlayer sender, object args)
-        {
-            switch (sender.CurrentState)
-            {
-                case MediaPlayerState.Playing:
-                    await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                    {
-                        playButton.Content = "| |";     // Change to pause button
-                        prevButton.IsEnabled = true;
-                        nextButton.IsEnabled = true;
-                    }
-                        );
-
-                    break;
-                case MediaPlayerState.Paused:
-                    await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                    {
-                        playButton.Content = ">";     // Change to play button
-                    }
-                    );
-
-                    break;
-            }
-        }
-
-        /// <summary>
-        /// This event fired when a message is received from Background Process
-        /// </summary>
-        async void BackgroundMediaPlayer_MessageReceivedFromBackground(object sender, MediaPlayerDataReceivedEventArgs e)
-        {
-            foreach (string key in e.Data.Keys)
-            {
-                switch (key)
-                {
-                    case Constants.Trackchanged:
-                        //When foreground app is active change track based on background message
-                        await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-                        {
-                            txtCurrentTrack.Text = (string)e.Data[key];
-                        }
-                        );
-                        break;
-                    case Constants.BackgroundTaskStarted:
-                        //Wait for Background Task to be initialized before starting playback
-                        Debug.WriteLine("Background Task started");
-                        ServerInitialized.Set();
-                        break;
-                }
-            }
-        }
-
-        #endregion
-
-        #region Button Click Event Handlers
-        /// <summary>
-        /// Sends message to the background task to skip to the previous track.
-        /// </summary>
-        private void prevButton_Click(object sender, RoutedEventArgs e)
-        {
-            var value = new ValueSet();
-            value.Add(Constants.SkipPrevious, "");
-            BackgroundMediaPlayer.SendMessageToBackground(value);
-
-            // Prevent the user from repeatedly pressing the button and causing 
-            // a back-long of button presses to be handled. This button is re-enabled 
-            // in the TrackReady Play state handler.
-            prevButton.IsEnabled = false;
-        }
-
-        /// <summary>
-        /// If the task is already running, it will just play/pause MediaPlayer Instance
-        /// Otherwise, initializes MediaPlayer Handlers and starts playback
-        /// track or to pause if we're already playing.
-        /// </summary>
-        private void playButton_Click(object sender, RoutedEventArgs e)
-        {
-            Debug.WriteLine("Play button pressed from App");
-            if (IsMyBackgroundTaskRunning)
-            {
-                if (MediaPlayerState.Playing == BackgroundMediaPlayer.Current.CurrentState)
-                {
-                    BackgroundMediaPlayer.Current.Pause();
-                }
-                else if (MediaPlayerState.Paused == BackgroundMediaPlayer.Current.CurrentState)
-                {
-                    BackgroundMediaPlayer.Current.Play();
-                }
-                else if (MediaPlayerState.Closed == BackgroundMediaPlayer.Current.CurrentState)
-                {
-                    StartBackgroundAudioTask();
-                }
-            }
-            else
-            {
-                StartBackgroundAudioTask();
-            }
-        }
-
-        /// <summary>
-        /// Tells the background audio agent to skip to the next track.
-        /// </summary>
-        /// <param name="sender">The button</param>
-        /// <param name="e">Click event args</param>
-        private void nextButton_Click(object sender, RoutedEventArgs e)
-        {
-            var value = new ValueSet();
-            value.Add(Constants.SkipNext, "");
-            BackgroundMediaPlayer.SendMessageToBackground(value);
-
-            // Prevent the user from repeatedly pressing the button and causing 
-            // a back-long of button presses to be handled. This button is re-enabled 
-            // in the TrackReady Play-state handler.
-            nextButton.IsEnabled = false;
-        }
-
-        #endregion Button Click Event Handlers
-
-        #region Media Playback Helper methods
-        /// <summary>
-        /// Unsubscribes to MediaPlayer events. Should run only on suspend
-        /// </summary>
-        private void RemoveMediaPlayerEventHandlers()
-        {
-            BackgroundMediaPlayer.Current.CurrentStateChanged -= this.MediaPlayer_CurrentStateChanged;
-            BackgroundMediaPlayer.MessageReceivedFromBackground -= this.BackgroundMediaPlayer_MessageReceivedFromBackground;
-        }
-
-        /// <summary>
-        /// Subscribes to MediaPlayer events
-        /// </summary>
-        private void AddMediaPlayerEventHandlers()
-        {
-            BackgroundMediaPlayer.Current.CurrentStateChanged += this.MediaPlayer_CurrentStateChanged;
-            BackgroundMediaPlayer.MessageReceivedFromBackground += this.BackgroundMediaPlayer_MessageReceivedFromBackground;
-        }
-
-        /// <summary>
-        /// Initialize Background Media Player Handlers and starts playback
-        /// </summary>
-        private void StartBackgroundAudioTask()
-        {
-            AddMediaPlayerEventHandlers();
-            var backgroundtaskinitializationresult = this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
-            {
-                bool result = ServerInitialized.WaitOne(2000);
-                //Send message to initiate playback
-                if (result == true)
-                {
-                    var message = new ValueSet();
-                    message.Add(Constants.StartPlayback, "0");
-                    BackgroundMediaPlayer.SendMessageToBackground(message);
-                }
-                else
-                {
-                    throw new Exception("Background Audio Task didn't start in expected time");
-                }
-            }
-            );
-            backgroundtaskinitializationresult.Completed = new AsyncActionCompletedHandler(BackgroundTaskInitializationCompleted);
-        }
-
-        private void BackgroundTaskInitializationCompleted(IAsyncAction action, AsyncStatus status)
-        {
-            if (status == AsyncStatus.Completed)
-            {
-                Debug.WriteLine("Background Audio Task initialized");
-            }
-            else if (status == AsyncStatus.Error)
-            {
-                Debug.WriteLine("Background Audio Task could not initialized due to an error ::" + action.ErrorCode.ToString());
-            }
-        }
-        #endregion
     }
 }
--- AsotListener.App/ViewModels/PlayerViewModel.cs
+++ AsotListener.App/ViewModels/PlayerViewModel.cs
@@ -1,14 +1,329 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.Media.Playback;
-
-namespace AsotListener.App.ViewModels
+﻿namespace AsotListener.App.ViewModels
 {
-    class PlayerViewModel
+    using System;
+    using System.Diagnostics;
+    using System.Threading;
+    using System.Windows.Input;
+    using Models;
+    using Services;
+    using Windows.Foundation;
+    using Windows.Foundation.Collections;
+    using Windows.Media.Playback;
+    using Windows.UI.Xaml.Controls;
+
+    public class PlayerViewModel : BaseModel, IDisposable
     {
-        MediaPlayer pl = BackgroundMediaPlayer.Current;
+        #region Fields
+
+        private MediaPlayer mediaPlayer;
+        private bool isPreviousButtonEnabled;
+        private bool isNextButtonEnabled;
+        private bool isPlayButtonEnabled;
+        private IconElement pauseIcon = new SymbolIcon(Symbol.Pause);
+        private IconElement playIcon = new SymbolIcon(Symbol.Play);
+        private IconElement playButtonIcon;
+        private bool isMyBackgroundTaskRunning = false;
+        private IApplicationSettingsHelper applicationSettingsHelper;
+        private AutoResetEvent ServerInitialized;
+
+        #endregion
+
+        #region Properties
+
+        public bool IsPreviousButtonEnabled
+        {
+            get { return isPreviousButtonEnabled; }
+            set { SetField(ref isPreviousButtonEnabled, value, nameof(IsPreviousButtonEnabled)); }
+        }
+
+        public bool IsNextButtonEnabled
+        {
+            get { return isNextButtonEnabled; }
+            set { SetField(ref isNextButtonEnabled, value, nameof(IsNextButtonEnabled)); }
+        }
+
+        public bool IsPlayButtonEnabled
+        {
+            get { return isPlayButtonEnabled; }
+            set { SetField(ref isPlayButtonEnabled, value, nameof(IsPlayButtonEnabled)); }
+        }
+
+        public IconElement PlayButtonIcon
+        {
+            get { return playButtonIcon; }
+            set { SetField(ref playButtonIcon, value, nameof(PlayButtonIcon)); }
+        }
+
+        public ICommand PreviousTrackCommand { get; }
+        public ICommand NextTrackCommand { get; }
+        public ICommand PlayPauseCommand { get; }
+
+        public IPlayList Playlist { get; }
+
+        #endregion
+
+        #region Ctor
+
+        public PlayerViewModel()
+        {
+            mediaPlayer = BackgroundMediaPlayer.Current;
+            Playlist = Services.Playlist.Instance;
+            applicationSettingsHelper = ApplicationSettingsHelper.Instance;
+
+            PlayButtonIcon = playIcon;
+
+            // Using explicit casts here because of http://stackoverflow.com/questions/2057146/compiler-ambiguous-invocation-error-anonymous-method-and-method-group-with-fun
+            PreviousTrackCommand = new RelayCommand((Action)onPreviousTrackAction);
+            NextTrackCommand = new RelayCommand((Action)onNextButtonAction);
+            PlayPauseCommand = new RelayCommand((Action)onPlayPauseAction);
+
+            updateBackgroundTaskRunningStatus();
+            ServerInitialized = new AutoResetEvent(false);
+
+            //Adding App suspension handlers here so that we can unsubscribe handlers 
+            //that access to BackgroundMediaPlayer events
+            App.Current.Suspending += ForegroundApp_Suspending;
+            App.Current.Resuming += ForegroundApp_Resuming;
+        }
+
+        #endregion
+
+        #region Foreground App Lifecycle Handlers
+        
+        /// <summary>
+        /// Sends message to background informing app has resumed
+        /// Subscribe to MediaPlayer events
+        /// </summary>
+        void ForegroundApp_Resuming(object sender, object e)
+        {
+            applicationSettingsHelper.SaveSettingsValue(Constants.AppState, Constants.ForegroundAppActive);
+
+            // Verify if the task was running before
+            updateBackgroundTaskRunningStatus();
+            if (isMyBackgroundTaskRunning)
+            {
+                //if yes, reconnect to media play handlers
+                AddMediaPlayerEventHandlers();
+
+                //send message to background task that app is resumed, so it can start sending notifications
+                ValueSet message = new ValueSet() { { Constants.AppResumed, DateTime.Now.ToString() } };
+                BackgroundMediaPlayer.SendMessageToBackground(message);
+
+                if (mediaPlayer.CurrentState == MediaPlayerState.Playing)
+                {
+                    PlayButtonIcon = pauseIcon;
+                }
+                else
+                {
+                    PlayButtonIcon = playIcon;
+                }
+            }
+            else
+            {
+                PlayButtonIcon = playIcon;
+            }
+
+        }
+
+        /// <summary>
+        /// Send message to Background process that app is to be suspended
+        /// Stop clock and slider when suspending
+        /// Unsubscribe handlers for MediaPlayer events
+        /// </summary>
+        void ForegroundApp_Suspending(object sender, Windows.ApplicationModel.SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            ValueSet message = new ValueSet() { { Constants.AppSuspended, DateTime.Now.ToString() } };
+            BackgroundMediaPlayer.SendMessageToBackground(message);
+            RemoveMediaPlayerEventHandlers();
+            applicationSettingsHelper.SaveSettingsValue(Constants.AppState, Constants.ForegroundAppSuspended);
+            deferral.Complete();
+        }
+        
+        #endregion
+
+        #region Button Commands
+
+        /// <summary>
+        /// Sends message to the background task to skip to the previous track.
+        /// </summary>
+        private void onPreviousTrackAction()
+        {
+            var value = new ValueSet() { { Constants.SkipPrevious, string.Empty } };
+            BackgroundMediaPlayer.SendMessageToBackground(value);
+
+            // Prevent the user from repeatedly pressing the button and causing 
+            // a back-long of button presses to be handled. This button is re-enabled 
+            // in the TrackReady Play state handler.
+            IsPreviousButtonEnabled = false;
+        }
+
+        /// <summary>
+        /// If the task is already running, it will just play/pause MediaPlayer Instance
+        /// Otherwise, initializes MediaPlayer Handlers and starts playback
+        /// track or to pause if we're already playing.
+        /// </summary>
+        private void onPlayPauseAction()
+        {
+            Debug.WriteLine("Play button pressed from App");
+
+            // Play button will be enabled when media player will be ready
+            IsPlayButtonEnabled = false;
+            updateBackgroundTaskRunningStatus();
+            if (isMyBackgroundTaskRunning)
+            {
+                if (MediaPlayerState.Playing == BackgroundMediaPlayer.Current.CurrentState)
+                {
+                    BackgroundMediaPlayer.Current.Pause();
+                }
+                else if (MediaPlayerState.Paused == BackgroundMediaPlayer.Current.CurrentState)
+                {
+                    BackgroundMediaPlayer.Current.Play();
+                }
+                else if (MediaPlayerState.Closed == BackgroundMediaPlayer.Current.CurrentState)
+                {
+                    StartBackgroundAudioTask();
+                }
+            }
+            else
+            {
+                StartBackgroundAudioTask();
+            }
+        }
+
+        /// <summary>
+        /// Tells the background audio agent to skip to the next track.
+        /// </summary>
+        private void onNextButtonAction()
+        {
+            var value = new ValueSet() { { Constants.SkipNext, string.Empty } };
+            BackgroundMediaPlayer.SendMessageToBackground(value);
+
+            // Prevent the user from repeatedly pressing the button and causing 
+            // a back-long of button presses to be handled. This button is re-enabled 
+            // in the TrackReady Play-state handler.
+            IsNextButtonEnabled = false;
+        }
+
+        #endregion
+
+        #region Background MediaPlayer Event handlers
+
+        /// <summary>
+        /// MediaPlayer state changed event handlers. 
+        /// Note that we can subscribe to events even if Media Player is playing media in background
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void MediaPlayer_CurrentStateChanged(MediaPlayer sender, object args)
+        {
+            IsPlayButtonEnabled = true;
+            switch (sender.CurrentState)
+            {
+                case MediaPlayerState.Playing:
+                    PlayButtonIcon = pauseIcon;
+                    IsNextButtonEnabled = true;
+                    IsPreviousButtonEnabled = true;
+                    break;
+                case MediaPlayerState.Paused:
+                    PlayButtonIcon = playIcon;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// This event fired when a message is received from Background Process
+        /// </summary>
+        void BackgroundMediaPlayer_MessageReceivedFromBackground(object sender, MediaPlayerDataReceivedEventArgs e)
+        {
+            foreach (string key in e.Data.Keys)
+            {
+                if (key == Constants.BackgroundTaskStarted)
+                {
+                    //Wait for Background Task to be initialized before starting playback
+                    Debug.WriteLine("Background Task started");
+                    ServerInitialized.Set();
+                    return;
+                }
+            }
+        }
+
+        #endregion
+
+        #region Helper Methods
+        private void updateBackgroundTaskRunningStatus()
+        {
+            object value = applicationSettingsHelper.ReadSettingsValue(Constants.BackgroundTaskState);
+            if (value == null)
+            {
+                isMyBackgroundTaskRunning = false;
+            }
+
+            isMyBackgroundTaskRunning = ((string)value).Equals(Constants.BackgroundTaskRunning);
+        }
+
+        /// <summary>
+        /// Unsubscribes to MediaPlayer events. Should run only on suspend
+        /// </summary>
+        private void RemoveMediaPlayerEventHandlers()
+        {
+            BackgroundMediaPlayer.Current.CurrentStateChanged -= this.MediaPlayer_CurrentStateChanged;
+            BackgroundMediaPlayer.MessageReceivedFromBackground -= this.BackgroundMediaPlayer_MessageReceivedFromBackground;
+        }
+
+        /// <summary>
+        /// Subscribes to MediaPlayer events
+        /// </summary>
+        private void AddMediaPlayerEventHandlers()
+        {
+            BackgroundMediaPlayer.Current.CurrentStateChanged += this.MediaPlayer_CurrentStateChanged;
+            BackgroundMediaPlayer.MessageReceivedFromBackground += this.BackgroundMediaPlayer_MessageReceivedFromBackground;
+        }
+
+        /// <summary>
+        /// Initialize Background Media Player Handlers and starts playback
+        /// </summary>
+        private void StartBackgroundAudioTask()
+        {
+            AddMediaPlayerEventHandlers();
+            bool result = ServerInitialized.WaitOne(2000);
+            if (result == true)
+            {
+                var message = new ValueSet() { { Constants.StartPlayback, "0" } };
+                BackgroundMediaPlayer.SendMessageToBackground(message);
+            }
+            else
+            {
+                throw new Exception("Background Audio Task didn't start in expected time");
+            }
+        }
+
+        #endregion
+
+        #region IDisposable Support
+
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    this.ServerInitialized.Dispose();
+                }                
+
+                disposedValue = true;
+            }
+        }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+        }
+
+        #endregion
     }
 }
