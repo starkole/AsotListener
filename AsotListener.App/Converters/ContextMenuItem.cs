a15907fb824a98eda06d07db62747179948bd3fa
Pavlo Oleshkevych <oleshkevych@gmail.com>
2015-12-08 10:44:22 +0200

    Implemented context menu visibility converter.

---
+++ AsotListener.App/Converters/ContextMenuItem.cs
@@ -0,0 +1,11 @@
+﻿namespace AsotListener.App.Converters
+{
+    public enum ContextMenuItem
+    {
+        Download,
+        CancelDownload,
+        Delete,
+        Play,
+        AddToPlaylist
+    }
+}
--- AsotListener.App/Converters/ContextMenuVisibilityConverter.cs
+++ AsotListener.App/Converters/ContextMenuVisibilityConverter.cs
@@ -1,27 +1,61 @@
 ﻿namespace AsotListener.App.Converters
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
     using Models;
     using Windows.UI.Xaml;
     using Windows.UI.Xaml.Data;
 
     public class ContextMenuVisibilityConverter : IValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, string language)
+        public object Convert(object value, Type targetType, object itemType, string language)
         {
+            Visibility result = Visibility.Collapsed;
             Episode episode = value as Episode;
-            if(episode == null)
+            ContextMenuItem menuItem;
+            bool isItemTypeValid = Enum.TryParse(itemType.ToString(), out menuItem);
+            if(episode == null || !isItemTypeValid)
             {
-                return Visibility.Collapsed;
+                return result;
             }
 
-            // TODO: Implement visibility checking logic here
+            
+            switch (menuItem)
+            {   
+                case ContextMenuItem.Download:
+                    if (episode.Status == EpisodeStatus.CanBeLoaded)
+                    {
+                        result = Visibility.Visible;
+                    }
+                    break;
+                case ContextMenuItem.CancelDownload:
+                    if (episode.Status == EpisodeStatus.Downloading)
+                    {
+                        result = Visibility.Visible;
+                    }
+                    break;
+                case ContextMenuItem.Delete:
+                    if (episode.Status == EpisodeStatus.Loaded)
+                    {
+                        result = Visibility.Visible;
+                    }
+                    break;
+                case ContextMenuItem.Play:
+                    if (episode.Status == EpisodeStatus.Loaded)
+                    {
+                        result = Visibility.Visible;
+                    }
+                    break;
+                case ContextMenuItem.AddToPlaylist:
+                    if (episode.Status == EpisodeStatus.Loaded)
+                    {
+                        result = Visibility.Visible;
+                    }
+                    break;
+                default:
+                    break;
+            }
 
-            return Visibility.Visible;
+            return result;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language) => null;
