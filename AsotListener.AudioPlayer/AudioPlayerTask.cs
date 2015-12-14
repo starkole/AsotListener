c605c2168fd144f113c1853eb716187b0b76f7b8
Pavlo Oleshkevych <oleshkevych@gmail.com>
2015-12-14 17:34:55 +0200

    Renamed background audio player to AudioPlayerTask.

--- AsotListener.AudioPlayer/AudioPlayerTask.cs
+++ AsotListener.AudioPlayer/AudioPlayerTask.cs
@@ -33,7 +33,7 @@
     /// Impalements IBackgroundTask to provide an entry point for app code to be run in background. 
     /// Also takes care of handling UVC and communication channel with foreground
     /// </summary>
-    public sealed class AudioPlayer : IBackgroundTask, IDisposable
+    public sealed class AudioPlayerTask : IBackgroundTask, IDisposable
     {
         #region Private fields
 
